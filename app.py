8ac25c359fd8360e06ee4f6919e7d0d1a676abd4
Jayanth2323 <jayanthchennoju@gmail.com>
2025-06-20 23:21:50 +0530

    Add application file

--- app.py
+++ app.py
@@ -1,20 +1,27 @@
 # app.py
 import gradio as gr
 import joblib
-import numpy as np
+
+# import numpy as np
 import pandas as pd
 
 model = joblib.load("model/TCS_Stock_Predictor.pkl")
 
-def predict(open, high, low):
-    data = pd.DataFrame([[open, high, low]], columns=['Open', 'High', 'Low'])
+
+def predict(high, low):
+    data = pd.DataFrame([[high, low]], columns=["Open", "High", "Low"])
     prediction = model.predict(data)
     return f"Predicted Close Price: {prediction[0]:.2f}"
 
+
 iface = gr.Interface(
     fn=predict,
-    inputs=[gr.Number(label="Open"), gr.Number(label="High"), gr.Number(label="Low")],
-    outputs="text"
+    inputs=[
+        gr.Number(label="Open"),
+        gr.Number(label="High"),
+        gr.Number(label="Low")
+        ],
+    outputs="text",
 )
 
 if __name__ == "__main__":
